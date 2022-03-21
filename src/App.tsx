220bd511a3efd1530b92b7ae42a24f2da68ccbbe
yangyi <iyoung2018@126.com>
2022-03-21 22:01:33 +0800

    3.21

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import
+import TodoTextInput from './component/todoTextInput'
 
-  class App extends React.Component<AppProps> {
-    render() {
-      const { todos, dispatch } = this.props;
+// const mapStateToProps = state => ({
+//   todos: state.todos
+// });
 
-      return (
-        <div className="todoapp">
-          <Header addTodo={(text: string) => dispatch(addTodo(text))} />
-          <MainSection
-            todos={todos}
-            editTodo={(t, s) => dispatch(editTodo(t, s))}
-            deleteTodo={(t: model.Todo) => dispatch(deleteTodo(t))}
-            completeTodo={(t: model.Todo) => dispatch(completeTodo(t))}
-            clearCompleted={() => dispatch(clearCompleted())}
-            completeAll={() => dispatch(completeAll())} />
-        </div>
-      );
-    }
-  }
-
-const mapStateToProps = state => ({
-  todos: state.todos
-});
-
-export default connect(mapStateToProps)(App);
+export default TodoTextInput
